476bf191c5993fd0f3acd26fe769c1af13919780
张金虎 <1151916951@qq.com>
2019-06-21 12:34:19 +0800

    配置fastdfs

--- src/components/user/studentList.vue
+++ src/components/user/studentList.vue
@@ -3,7 +3,7 @@
     <el-col :span="24" class="warp-breadcrum">
       <el-breadcrumb separator="/">
         <el-breadcrumb-item :to="{ path: '/' }"><b>首页</b></el-breadcrumb-item>
-        <el-breadcrumb-item>教师列表</el-breadcrumb-item>
+        <el-breadcrumb-item>学生列表</el-breadcrumb-item>
       </el-breadcrumb>
     </el-col>
 
